56748eac8a53e982ceeae47bb9bc033f7c64e6a3
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-06-08 11:10:32 +0700

    filled todo - adding function bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+// return array of data
+// data1.json => halo dunia => hasil yang diambil adalah dunia
+// data2.json => halo world => hasil yang diambil adalah world
+// data3.json => halo sekai => hasil yang diambli adalah sekai
+const bacaData = (fnCallback) => {
+  Promise.all([
+    new Promise((resolve, reject) => {
+      fs.readFile(file1, "utf8", (err, data) => {
+        if (err) reject(err);
+        resolve(JSON.parse(data));
+      });
+    }),
+    new Promise((resolve, reject) => {
+      fs.readFile(file2, "utf8", (err, data) => {
+        if (err) reject(err);
+        resolve(JSON.parse(data));
+      });
+    }),
+    new Promise((resolve, reject) => {
+      fs.readFile(file3, "utf8", (err, data) => {
+        if (err) reject(err);
+        resolve(JSON.parse(data));
+      });
+    }),
+  ]).then((data) => {
+    const result = data.map((item) => {
+      return JSON.stringify(item)
+        .replace(/([^a-zA-Z]|data|message|halo)/gi, "")
+        .trim();
+    });
+    fnCallback(null, result);
+  }).catch((err) => {
+    fnCallback(err, null);
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
